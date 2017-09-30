5b584e7c1df07a8850b4a4caf5721cfe6f5ac200
Mike <mkoval3@gatech.edu>
2017-09-30 17:02:56 -0400

    added new project

---
+++ Koval_M_m1/Assets/Scripts/AudioEventManager.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class AudioEventManager : MonoBehaviour
+{
+
+    public AudioClip boxAudio;
+    public AudioClip playerLandsAudio;
+	public AudioClip playerFootstepsGravelAudio;
+	public AudioClip playerFootstepsSnowAudio;
+
+
+
+    private UnityAction<Vector3> boxCollisionEventListener;
+
+    private UnityAction<Vector3> playerLandsEventListener;
+	private UnityAction<Vector3> playerFootstepsGravelEventListener;
+	private UnityAction<Vector3> playerFootstepsSnowEventListener;
+
+
+
+   
+
+    void Awake()
+    {
+
+        boxCollisionEventListener = new UnityAction<Vector3>(boxCollisionEventHandler);
+
+        playerLandsEventListener = new UnityAction<Vector3>(playerLandsEventHandler);
+
+		playerFootstepsGravelEventListener = new UnityAction<Vector3>(playerFootstepsGravelEventHandler);
+		playerFootstepsSnowEventListener = new UnityAction<Vector3>(playerFootstepsSnowEventHandler);
+
+    }
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+
+        			
+    }
+
+
+    void OnEnable()
+    {
+
+        EventManager.StartListening<BoxCollisionEvent, Vector3>(boxCollisionEventListener);
+        EventManager.StartListening<PlayerLandsEvent, Vector3>(playerLandsEventListener);
+		EventManager.StartListening<PlayerFootstepsGravelEvent, Vector3>(playerFootstepsGravelEventListener);
+		EventManager.StartListening<PlayerFootstepSnowEvent, Vector3>(playerFootstepsSnowEventListener);
+    }
+
+    void OnDisable()
+    {
+
+        EventManager.StopListening<BoxCollisionEvent, Vector3>(boxCollisionEventListener);
+        EventManager.StopListening<PlayerLandsEvent, Vector3>(playerLandsEventListener);
+		EventManager.StopListening<PlayerFootstepSnowEvent, Vector3>(playerFootstepsSnowEventListener);
+		EventManager.StopListening<PlayerFootstepsGravelEvent, Vector3>(playerFootstepsGravelEventListener);
+    }
+
+
+	
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+
+ 
+
+    void boxCollisionEventHandler(Vector3 worldPos)
+    {
+        AudioSource.PlayClipAtPoint(this.boxAudio, worldPos);
+    }
+
+    void playerLandsEventHandler(Vector3 worldPos)
+    {
+        AudioSource.PlayClipAtPoint(this.playerLandsAudio, worldPos);
+    }
+	void playerFootstepsGravelEventHandler(Vector3 worldPos)
+	{
+		AudioSource.PlayClipAtPoint(this.playerFootstepsGravelAudio, worldPos);
+	}
+	void playerFootstepsSnowEventHandler(Vector3 worldPos)
+	{
+		AudioSource.PlayClipAtPoint(this.playerFootstepsSnowAudio, worldPos);
+	}
+
+}
---
+++ Koval_M_m1/Koval_M_m1_readme.txt
@@ -0,0 +1,10 @@
+Michael Koval
+mkoval3@gatech.edu
+902929396
+
+I used the git hub project and expanded on what was given to us. I imported my own character that I created in Fuse, added the additional animations (all should be there). I blended them together.
+I used curves for the footsteps however I had some problems with the lowest speeds but it worked decently well. 
+
+I used an animation mask for the throwing so you can throw while running however the arms feel like they could have been done better
+
+The scene should be the same scene given in the github project.
